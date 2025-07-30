de3166c126729bbaf58059e90a47df31428ba8a6
Yaqot <user@users-MacBook-Pro.local>
2025-07-30 11:54:35 +0300

    change on the cors

--- server/index.js
+++ server/index.js
@@ -7,12 +7,24 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://madina-ecommerce.vercel.app",
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://madina-ecommerce.vercel.app"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json()); // VERY IMPORTANT
 
 // DB connection
