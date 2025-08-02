25c0eb94089a131c15c88e78405f59bb16787389
Yaqot <user@users-MacBook-Pro.local>
2025-08-02 12:43:25 +0300

    add SpeedInsights

--- client/ama/src/main.tsx
+++ client/ama/src/main.tsx
@@ -1,20 +1,19 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "@/context/CartContext";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </CartProvider>
-    </AuthProvider>
-  </StrictMode>
+  <AuthProvider>
+    <CartProvider>
+      <BrowserRouter>
+        <SpeedInsights />
+        <App />
+      </BrowserRouter>
+    </CartProvider>
+  </AuthProvider>
 );
