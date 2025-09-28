bdaac7460d009321d6f0e003dd9e5c9733572aae
Ahmad Salous <ahmadbasemsalos@gmail.com>
2025-09-28 10:36:03 +0300

    hide the offers banner and dialog

--- client/ama/src/components/Navbar.tsx
+++ client/ama/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CartButton from "@/components/CartButton";
 import { Button } from "@/components/ui/button";
